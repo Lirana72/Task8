688dfaa5379da9c1ea6a21fc0db3f3edcb8fa868
falenag@gmail.com <falenag@gmail.com>
2023-06-17 19:31:45 +0900

    Добавьте файлы проекта.

---
+++ Task8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputD = "Введите  число";
+            Console.WriteLine(InputD);
+            int a = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= a; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i.ToString() + " ");
+                }
+            }
+            Console.Write("");
+        }
+    }
+}
